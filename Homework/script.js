06227d8b190e425338e9ab591abac89bb1d15894
Alex <piatovolenkoo@gmail.com>
2019-06-15 18:40:14 +0300

    First

---
+++ Homework/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.main_btn, .main_btna, a[href = "#sheldure"]').on('click', function(){
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+
+    $('.close').on('click', function(){
+        $('.overlay').fadeOut(1000);
+        $('.modal').slideUp(1000);
+    });
+
+});
